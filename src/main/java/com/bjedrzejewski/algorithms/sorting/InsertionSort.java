d388e6d67895340d711413bf9363a4edc63e119f
bartoszjedrzejewski <jedrus07@gmail.com>
2016-08-15 22:49:06 +0100

    Commented the insertion sort.

--- src/main/java/com/bjedrzejewski/algorithms/sorting/InsertionSort.java
+++ src/main/java/com/bjedrzejewski/algorithms/sorting/InsertionSort.java
@@ -16,16 +16,20 @@ public final class InsertionSort {
      * @param input
      */
     public static void sort(int[] input){
+        //Arrays of size 0 and 1 are already sorted.
         if(input.length < 2){
             return;
         }
+        //We start with the second element, as we treat the first element as an already sorted array.
         for(int j = 1; j < input.length; j++){
             int key = input[j];
+            //We are trying to efficiently insert input[j] element into the [0 ... j-1] array.
             int i = j - 1;
             while(i >= 0 && input[i] > key){
                 input[i + 1] = input[i];
                 i = i - 1;
             }
+            //Once input[i] > input[j] or can't find any, we found the correct place.
             input[i+1] = key;
         }
     }
